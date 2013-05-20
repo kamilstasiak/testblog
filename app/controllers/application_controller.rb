b829a9d1088eb956cd6b2179f88d2ab0cb596874
Kamil Stasiak <kamil.stasiak1@gmail.com>
2013-05-20 23:08:47 +0200

    Disable flash on index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :display_flash_notice
+  before_filter :display_flash_notice, :except => "index"
 
   private
 
